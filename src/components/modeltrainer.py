62b6b84716a3d407c75c24bc1628ec0527089fb4
narayanaraghavendra <136465583+narayanaraghavendra@users.noreply.github.com>
2023-10-20 21:36:49 +0530

    Updated  modeltrainer.py

--- src/components/modeltrainer.py
+++ src/components/modeltrainer.py
@@ -13,9 +13,11 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
+from src.logger import logging
+
 
 from src.exception import CustomException
-from src.logger import logging
+
 
 from src.utils import save_object,evaluate_models
 
